d7aaeb97cf2de494914cfe9b640b9ae4efa59799
Michael Kim <m.kim4247@gmail.com>
2018-11-05 17:18:42 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -11,13 +11,37 @@ class SongsController < ApplicationController
     @song = Song.new
   end
 
+  def create
+    @song = Song.new(song_params)
+
+    if @song.save
+      redirect_to @song
+    else
+      render :new
+    end
+  end
+
   def edit
     @song = Song.find(params[:id])
   end
 
-  def create
-    song = Song.create(song_params)
-    redirect_to controller: 'songs', action: 'index'
+  def update
+    @song = Song.find(params[:id])
+
+    @song.update(song_params)
+
+    if @song.save
+      redirect_to @song
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @song = Song.find(params[:id])
+    @song.destroy
+    flash[:notice] = "Song deleted."
+    redirect_to songs_path
   end
 
   private
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,39 +1,37 @@
 class Song < ActiveRecord::Base
   # add associations here
   belongs_to :artist
-    belongs_to :genre
-    has_many :notes
+  belongs_to :genre
+  has_many :notes
 
-    def genre_name=(name)
-      genre = Genre.find_or_create_by(name: name)
-      self.genre = genre
-    end
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
 
-    def genre_name
-      self.try(:genre).try(:name)
-    end
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
 
-    def artist_name
-      self.try(:artist).try(:name)
-    end
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
 
-    def artist_name=(name)
-      artist = Artist.find_or_create_by(name: name)
-      self.artist = artist
-    end
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
 
-    def note_contents=(notes)
-      notes.each do |content|
-        if content.strip != ''
-          self.notes.build(content: content)
-        end
+  def note_contents=(notes)
+    notes.each do |n|
+      if n != ""
+      self.notes.build(content: n)
       end
     end
+  end
 
-    def note_contents
-      self.notes.map(&:content)
+  def note_contents
+    self.notes.collect do |n|
+      n.content
     end
-
-
+  end
 
 end
