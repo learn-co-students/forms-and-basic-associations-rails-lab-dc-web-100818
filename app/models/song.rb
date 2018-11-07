ec95306a5c2faf4217c69b4761cba2f50ec16255
jennyjean <jennyjean8675309@yahoo.com>
2018-11-07 09:58:03 -0500

    Created forms using basic assocations and nested data

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,43 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    if self.artist_id?
+      self.artist.name
+    else
+      nil
+    end
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_by(name: name)
+  end
+
+  def genre_name
+    if self.genre_id?
+      self.genre.name
+    else
+      nil
+    end
+  end
+
+  def note_contents=(notes)
+    notes.each do |note|
+      note = Note.create(content: note)
+      self.notes << note
+    end
+  end
+
+  def note_contents
+    notes = self.notes.collect do |note|
+        note.content
+    end
+    notes.select { |n| n != "" }
+  end
 end
